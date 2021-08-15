a6d6260eb898f3cb820c9b47dd0d5c71c3ea660d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-08-15 14:19:12 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # xray-websocket--no-tls
-基于ws实现免流
+
+## 警告⚠：此技术仅限用于个人搭建游戏加速器使用！！！若用于其他违法目的，后果自负！！！
+
+手动搭建xray服务器(vmess+websocket)，基于ws实现免流
+
+KVM/XEN机器执行(CentOS7):
+
+`yum install -y gcc wget && wget https://raw.githubusercontent.com/HXHGTS/xray-websocket-tls-nginx/main/v2ray.c -O v2ray.c && chmod +x v2ray.c && gcc -o v2ray v2ray.c && ./v2ray`
+
+第一次点击安装后会自动升级系统内核并触发重启，重启后输入
+
+`./v2ray`
+
+CDN套用方法请参看[CDN套用方法](/cdn.md)
+
+脚本生成的二维码可以用v2ray客户端扫描导入，VMESS链接可以直接导入v2ray客户端或在[这里](https://acl4ssr-sub.github.io/)生成Clash链接以导入Clash
+
+### 安装前准备：
+
+1.脚本仅支持CentOS7系统
+
+2.脚本执行需要root权限登录系统，AWS等特殊机型打开root权限方法[看这里](https://hxhgts.github.io/AWSECSRoot/)
+
+3.请在服务器后台防火墙放行tcp80(http)端口
---
+++ v2ray.c
@@ -0,0 +1,137 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+FILE* config;
+char uuid[40];
+int mode;
+
+int main(){
+Menu:UI();
+    system("clear");
+    if (mode == 1) {
+        install_xray();
+        goto Menu;
+    }
+    else if (mode == 2) {
+        system("systemctl stop xray");
+        system("systemctl start xray");
+        printf("正在检测xray运行状态，以下输出不为空则运行正常！\n");
+        printf("--------------以下输出不为空则xray运行正常------------------\n");
+        system("ss -lp | grep xray");
+        printf("--------------------------------------------------------\n");
+        goto Menu;
+    }
+    else if (mode == 3) {
+        config = fopen("/usr/local/etc/xray/uuid.conf", "r");
+        fscanf(config, "%s", uuid);
+        fclose(config);
+        printf("----------Server Info----------\n");
+        printf("port: 80\n");
+        printf("UUID: %s\n", uuid);
+        printf("alterId: 0\n");
+        printf("tls: false\n");
+        printf("network: ws\n");
+        printf("ws-path: /\n");
+        printf("Host: 自由修改免流参数\n");
+        printf("----------Server Info----------\n");
+        goto Menu;
+    }
+    else if (mode == 4) {
+        system("vi /usr/local/etc/xray/config.json");
+        system("systemctl restart xray");
+        printf("正在检测xray运行状态，以下输出不为空则运行正常！\n");
+        printf("--------------以下输出不为空则xray运行正常------------------\n");
+        system("ss -lp | grep xray");
+        printf("--------------------------------------------------------\n");
+        goto Menu;
+    }
+    else if (mode == 5) {
+        printf("正在更新xray主程序. . .\n");
+        system("systemctl stop xray");
+        system("curl -sSL https://cdn.jsdelivr.net/gh/XTLS/Xray-install/install-release.sh | sh");
+        system("systemctl start xray");
+        printf("xray主程序更新完成！\n");
+        printf("正在检测xray运行状态，以下输出不为空则运行正常！\n");
+        printf("--------------以下输出不为空则xray运行正常------------------\n");
+        system("ss -lp | grep xray");
+        printf("--------------------------------------------------------\n");
+        goto Menu;
+    }
+    else if (mode == 6) {
+        system("systemctl stop xray");
+        goto Menu;
+    }
+    else {
+        exit(0);
+    }
+    return 0;
+}
+
+int UI() {
+    printf("-----------------------------------------------------------\n");
+    printf("-----------------xray安装工具(80免流版)--------------------\n");
+    printf("-----------------------------------------------------------\n");
+    printf("----------------------当前Kernel版本-----------------------\n");
+    system("uname -sr");
+    printf("-----------------------------------------------------------\n");
+    printf("1.安装xray\n2.运行xray\n3.显示配置\n4.修改xray配置\n5.更新xray\n6.关闭xray\n0.退出\n");
+    printf("-----------------------------------------------------------\n");
+    printf("请输入:");
+    scanf("%d", &mode);
+    return 0;
+}
+
+int install_xray() {
+    KernelUpdate(); 
+    system("setenforce 0");
+    system("yum install -y uuidgen bind-utils");
+    printf("正在运行xray安装脚本. . .\n");
+    system("curl -sSL https://cdn.jsdelivr.net/gh/XTLS/Xray-install/install-release.sh | sh");
+    system("rm -rf TCPO.sh");
+    printf("正在生成配置文件. . .\n");
+    system("curl https://cdn.jsdelivr.net/gh/HXHGTS/xray-websocket-no-tls/config.json.1 > /usr/local/etc/xray/config.json");
+    printf("正在生成UUID. . .\n");
+    system("uuidgen > /usr/local/etc/xray/uuid.conf");
+    config = fopen("/usr/local/etc/xray/uuid.conf", "r");
+    fscanf(config, "%s", uuid);
+    fclose(config);
+    config = fopen("/usr/local/etc/xray/config.json", "a");
+    fprintf(config, "       \"id\": \"%s\"\n", uuid);
+    fclose(config);
+    system("curl https://cdn.jsdelivr.net/gh/HXHGTS/xray-websocket-no-tls/config.json.2 >> /usr/local/etc/xray/config.json");
+    printf("正在启动xray并将xray写入开机引导项. . .\n");
+    system("systemctl enable xray");
+    system("systemctl start xray");
+    system("setsebool -P httpd_can_network_connect 1");
+    printf("正在检测xray运行状态，以下输出不为空则运行正常！\n");
+    printf("--------------以下输出不为空则xray运行正常------------------\n");
+    system("ss -lp | grep xray");
+    printf("--------------------------------------------------------\n");
+    printf("----------Server Info----------\n");
+    printf("port: 80\n");
+    printf("UUID: %s\n", uuid);
+    printf("alterId: 0\n");
+    printf("tls: false\n");
+    printf("network: ws\n");
+    printf("ws-path: /\n");
+    printf("Host: 自由修改免流参数\n");
+    printf("----------Server Info----------\n\n");
+    printf("xray部署完成！\n");
+    return 0;
+}
+
+int KernelUpdate() {
+    if ((fopen("KernelUpdate.sh", "r")) == NULL) {
+        printf("正在升级新内核. . .\n");
+        system("wget https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/KernelUpdate.sh -O KernelUpdate.sh");
+        system("chmod +x KernelUpdate.sh");
+        printf("正在升级，将自动触发重启以应用配置. . .\n");
+        system("bash KernelUpdate.sh");
+    }
+    else {
+        system("curl -sSL https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/TCPO.sh | sh");
+    }
+    return 0;
+}
+
